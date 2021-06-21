6b50a718d8982a8cf849bf733f53c37fdbe29442
aaditya29 <mishraaaditya29@gmail.com>
2021-06-21 12:57:28 +0530

    Code Commit

--- Part 1/main.go
+++ Part 1/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	//Creating fucntion handler which prints hello world on any path request
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		log.Println("Hello World")
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+
 	http.ListenAndServe("9090", nil) //Binding all the addresses at port 9090
 }
