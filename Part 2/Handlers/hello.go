38f3ddda7cbccb4bb81dc3a1ea2ced2ae65c08c3
aaditya29 <mishraaaditya29@gmail.com>
2021-06-23 16:54:20 +0530

    Code Commit

--- Part 2/Handlers/hello.go
+++ Part 2/Handlers/hello.go
@@ -1,8 +1,15 @@
 package handlers
 
+import "net/http"
+
 //A handler responds to an HTTP request.
 //To create a handler we have to create a struct which implements interface http handler.
 
 //Hello is a simple handler
 type Hello struct {
+}
+
+//Method which satisfies HTTP handler interface
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) { //Signature that we need to satisfy the HTTP Interface
+
 }
