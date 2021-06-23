11a1f7de188ca2b4b87b23f53103d8b74217cdd8
aaditya29 <mishraaaditya29@gmail.com>
2021-06-23 13:21:35 +0530

    Comment Commit

--- Part 2/Handlers/hello.go
+++ Part 2/Handlers/hello.go
@@ -3,5 +3,6 @@ package handlers
 //A handler responds to an HTTP request.
 //To create a handler we have to create a struct which implements interface http handler.
 
+//Hello is a simple handler
 type Hello struct {
 }
