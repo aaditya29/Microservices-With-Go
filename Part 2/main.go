eb109b6c06f2ec3b32c1216d68d2a664ae20a676
aaditya29 <mishraaaditya29@gmail.com>
2021-07-02 10:31:55 +0530

    Code Commit

--- Part 2/main.go
+++ Part 2/main.go
@@ -10,5 +10,10 @@ func main() {
 	l := log.New(os.Stdout, "products-api ", log.LstdFlags) //Creating new logger
 	//Creating a reference to the handler
 	hh := handlers.NewHello(l)
+
+	// creating a new serve mux and registering the handlers
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
 	http.ListenAndServe(":9090", nil)
 }
