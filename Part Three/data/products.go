f13d661c4ba79830be56f9068b32626fc7c30edd
aaditya29 <mishraaaditya29@gmail.com>
2021-07-03 11:34:18 +0530

    Product List Added

--- Part Three/data/products.go
+++ Part Three/data/products.go
@@ -1,7 +1,10 @@
 package data
 
+import "time"
+
 // Defining the structure of API and properties inside
 type Product struct { //name of API is product
+	//statically defined product list
 	ID          int     `json:"id"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
@@ -10,4 +13,26 @@ type Product struct { //name of API is product
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
+}
+
+// example product list
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
 }
