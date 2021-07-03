9ef0e673ab94f520652c009eb47c2430e091f5a8
aaditya29 <mishraaaditya29@gmail.com>
2021-07-03 11:01:55 +0530

    Code Commit

---
+++ Part Three/data/products.go
@@ -0,0 +1,13 @@
+package data
+
+// Defining the structure of API and properties inside
+type Product struct { //name of API is product
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
