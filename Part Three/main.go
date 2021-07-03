a4257639f3c7a4519dc743889315b90c44cbea42
aaditya29 <mishraaaditya29@gmail.com>
2021-07-03 12:47:02 +0530

    Code Commit

--- Part Three/main.go
+++ Part Three/main.go
@@ -1,3 +1,16 @@
 // Building Restful services with GOlang
 // REST stands for Representational State Transfer Services
 // It is an architectural approach to design web services.
+
+package main
+
+import "net/http"
+
+func main() {
+	// create the handlers
+	ph := handlers.NewProducts(l)
+
+	// create a new serve mux and register the handlers
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+}
