4fdf8a22a3ec204b2cc7e15818813fd1736f4be5
aaditya29 <mishraaaditya29@gmail.com>
2021-07-03 11:58:51 +0530

    Code Commit

---
+++ Part Three/handlers/products..go
@@ -0,0 +1,13 @@
+package handlers
+
+import "log"
+
+// Products is a http.Handler
+type Products struct {
+	l *log.Logger
+}
+
+// NewProducts creates a products handler with the given logger
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
