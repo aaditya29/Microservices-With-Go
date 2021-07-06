1a5f118936e57a2e3813c71e9757e58bbb1de905
aaditya29 <mishraaaditya29@gmail.com>
2021-07-06 12:40:48 +0530

    Code Commit

--- Part_5/handlers/products..go
+++ Part_5/handlers/products..go
@@ -3,8 +3,6 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"regexp"
-	"strconv"
 
 	"github.com/aaditya29/Microservices-With-Go/tree/master/Part-Three/data"
 )
@@ -17,68 +15,6 @@ type Products struct {
 // NewProducts creates a products handler with the given logger
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
-}
-
-// ServeHTTP is the main entry point for the handler and staisfies the http.Handler
-// Making ServeHTTP function
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	// handle the request for a list of products
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
-	}
-
-	/*
-		Post creates a new resource at specified URL.
-		To add a new product we are going to implement POST operation
-	*/
-	//Adding new PRODUCTS
-	if r.Method == http.MethodPost {
-		p.addProduct(rw, r) //Passing responsewriter and request recieved
-		return
-	}
-
-	/*
-		PUT either creates or replaces the resource at specified URL.
-		The body of the request message specifies the resource to be created or updated.
-		Generally used for updating the resources
-	*/
-	//Updating the data
-	//Extracting the parameters from the URL
-	if r.Method == http.MethodPut {
-		p.l.Println("PUT", r.URL.Path)
-		// expect the id in the URI
-		//study the regex from golang documentation
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-
-		if len(g) != 1 {
-			p.l.Println("Invalid URI more than one id")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		if len(g[0]) != 2 {
-			p.l.Println("Invalid URI more than one capture group")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		idString := g[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			p.l.Println("Invalid URI unable to convert to numer", idString)
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		p.updateProducts(id, rw, r)
-		return
-	}
-
-	// catch all
-	// if no method is satisfied return an error
-	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 // getProducts returns the products from the data store
