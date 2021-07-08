ea5e2ab226d0ef31f961e7c8b414cd67ffd12b58
aaditya29 <mishraaaditya29@gmail.com>
2021-07-08 12:52:49 +0530

    Code Commit

---
+++ Part_6/handlers/products..go
@@ -0,0 +1,98 @@
+package handlers
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/aaditya29/Microservices-With-Go/tree/master/Part-Three/data"
+	"github.com/gorilla/mux"
+)
+
+// Products is a http.Handler
+type Products struct {
+	l *log.Logger
+}
+
+// NewProducts creates a products handler with the given logger
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	//we want to take getrequest and return product list from folder data
+	// we need encoding json for this stuff
+	p.l.Println("Handle GET Products")
+
+	//fetching the products from datastore
+	lp := data.GetProducts()
+
+	//serializing the list to JSON
+	err := lp.ToJSON(rw) //returning responsewriter
+	if err != nil {
+		http.Error(rw, "Unable to marshal json or fetch data", http.StatusInternalServerError)
+	}
+}
+
+//addProduct adds product on ever POST request
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&prod)
+}
+
+//Function to updateProduct
+func (p Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) //Passing a request
+
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Println("Handle PUT Product", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+
+	err = data.UpdateProduct(id, &prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
+}
+
+type KeyProduct struct{}
+
+func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler { /*
+		Mux supports the addition of middlewares to a Router, which are executed in the order they are added if a match is found,
+		including its subrouters.
+		 Middlewares are (typically) small pieces of code which take one request, do something with it,
+		 and pass it down to another middleware or the final handler.
+		  Some common use cases for middleware are request logging, header manipulation, or ResponseWriter hijacking.
+	*/
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
+			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		// add the product to the context
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(rw, r)
+	})
+}
