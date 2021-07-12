d96583d7ad622a3aec71e10bf6c3a750fe99fffa
aaditya29 <mishraaaditya29@gmail.com>
2021-07-12 12:18:08 +0530

    Code Commit

---
+++ Part_11/JWT Authentication/handlers.go
@@ -0,0 +1,18 @@
+package main
+
+// Writing the JWT Key used to create the signature
+var jwtKey = []byte("my_secret_key")
+
+// Handling User signin
+// It will take the users credentials and log them in.
+// Storing the users information as an in-memory map in our code
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+// Create a struct that models the structure of a user, both in the request body, and in the Database
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
