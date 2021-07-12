ed958ae491d4f0dcd4433578684b2b5bbc2ea9ae
aaditya29 <mishraaaditya29@gmail.com>
2021-07-12 12:30:14 +0530

    Code Commit

--- Part_11/JWT Authentication/handlers.go
+++ Part_11/JWT Authentication/handlers.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 // Writing the JWT Key used to create the signature
 var jwtKey = []byte("my_secret_key")
@@ -24,4 +30,40 @@ type Credentials struct {
 type Claims struct {
 	Username string `json:"username"`
 	jwt.StandardClaims
+}
+
+//Creating the Signin Handler
+func Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	// Get the JSON body and decode into credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		// If the structure of the body is wrong, return an HTTP error
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Get the expected password from our in memory map
+	expectedPassword, ok := users[creds.Username]
+
+	// If a password exists for the given user
+	// AND, if it is the same as the password we received, the we can move ahead
+	// if NOT, then we return an "Unauthorized" status
+	if !ok || expectedPassword != creds.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Declare the expiration time of the token
+	// here, we have kept it as 5 minutes
+	expirationTime := time.Now().Add(5 * time.Minute)
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
 }
