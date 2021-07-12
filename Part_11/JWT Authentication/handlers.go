4c3648365888ce0f4a328c2052f2b02bb49a9f56
aaditya29 <mishraaaditya29@gmail.com>
2021-07-12 12:31:38 +0530

    Code Commit

--- Part_11/JWT Authentication/handlers.go
+++ Part_11/JWT Authentication/handlers.go
@@ -66,4 +66,21 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+	// we also set an expiry time which is the same as the token itself
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
 }
